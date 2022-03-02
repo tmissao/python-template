5e5854055a415e7fb48bf37735c5a91b3375079f
Tiago Missão <tiago.missao@acqio.com.br>
2022-03-02 10:51:35 -0300

    fix environment boolean parse

--- app/config/config.py
+++ app/config/config.py
@@ -3,6 +3,6 @@ from distutils.util import strtobool
 
 
 class Config:
-    FLASK_DEBUG = bool(strtobool(os.environ.get('FLASK_DEBUG', False)))
+    FLASK_DEBUG = bool(strtobool(os.environ.get('FLASK_DEBUG', "False")))
     FLASK_PORT = os.environ.get('FLASK_PORT', 5000)
     FLASK_HOST = os.environ.get('FLASK_HOST', "0.0.0.0")
